2857b3927285aeeb9de41160a3161154b0956d08
Alif Hossain <alifhossain56782@gmail.com>
2024-08-28 18:38:44 +0600

    Complete Site Done

--- src/components/DoneDonation/DoneDonation.jsx
+++ src/components/DoneDonation/DoneDonation.jsx
@@ -6,7 +6,7 @@ const DoneDonation = ({donation}) => {
     return (
         <div className="md:w-3/4 p-4 space-y-4 rounded-xl w-full" style={{backgroundColor:card_bg}}>
           <img className="w-full" src={picture} alt="" />
-         <button style={{backgroundColor:text_button_bg}} className=" rounded-lg  px-2 text-base font-semibold text-white">{category}</button>
+          <button style={{backgroundColor:text_button_bg}} className=" rounded-lg  px-2 text-base font-semibold text-white">{category}</button>
           <h1 style={{color:title_text_color}} className=" text-xl font-bold">{title}</h1>
           <p className="font-bold text-xl">{price}</p>
         </div>
